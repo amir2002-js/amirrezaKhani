463897c1dbdb7975b68ffe492cfacb55edf9cbd2
masih abedini <masih7816@gmail.com>
2024-12-16 22:56:14 +0330

    feat: master changes - cahnge read data approach - refactor pretteirrc - change "Items" component to "Item" - debug app add to cart logic

--- src/pages/Basket/Basket.jsx
+++ src/pages/Basket/Basket.jsx
@@ -1,29 +1,41 @@
 import { getInShopBasket } from '../../js/localstorage';
-import Items from './Items';
-import { useEffect, useState } from 'react';
+import Item from './Item';
+import { useState } from 'react';
 import Order from './Order';
 import NotFound from '../../Components/404/NotFound';
 import Divider from '../../Components/Divider/Divider';
 
 export default function Basket({ products }) {
-    const productsInStorage = getInShopBasket();
-    const [rerender, setRerender] = useState(false);
-    
-    const [nums, setNums] = useState(
-        productsInStorage.map((i) => i.num),
+    const [productsInStorage, setProductsInStorage] = useState(() =>
+        getInShopBasket(),
     );
+
+    const nums = productsInStorage.map((i) => i.num);
+
+    const increaseProductCount = (id) => {
+        let products = JSON.parse(localStorage.getItem('shopBasket'));
+        const product = products.findIndex((item) => item.id == id);
+        if (product !== -1) {
+            products[product].num++;
+            localStorage.setItem('shopBasket', JSON.stringify(products));
+            setProductsInStorage(() => getInShopBasket());
+        }
+    };
+    const decreaseProductCount = (id) => {
+        let products = JSON.parse(localStorage.getItem('shopBasket'));
+        const product = products.findIndex((item) => item.id == id);
+        if (product !== -1) {
+            products[product].num--;
+            localStorage.setItem('shopBasket', JSON.stringify(products));
+            setProductsInStorage(() => getInShopBasket());
+        }
+    };
 
     let ids, basketProducts;
     if (productsInStorage && products) {
         ids = productsInStorage.map((i) => i.id);
-        basketProducts = products.filter((item) =>
-            ids.includes(item.id),
-        );
+        basketProducts = products.filter((item) => ids.includes(item.id));
     }
-
-    useEffect(() => {
-        setNums(productsInStorage.map((i) => i.num));
-    }, []);
 
     return (
         <div className="max-w-310 container">
@@ -38,57 +50,26 @@ export default function Basket({ products }) {
                             <div className="grid grid-cols-12 max-sm:grid-cols-1 justify-center items-center gap-10 w-full">
                                 <div className="border w-full border-black/15 shadow-lg rounded-3xl p-4 divide-y-2 flex flex-col gap-7 col-span-7 max-md:col-span-full">
                                     {basketProducts &&
-                                        basketProducts.map(
-                                            (p) => (
-                                                <Items
-                                                    productsInStorage={
-                                                        productsInStorage
-                                                    }
-                                                    key={
-                                                        p.id
-                                                    }
-                                                    id={
-                                                        p.id
-                                                    }
-                                                    price={
-                                                        p.price
-                                                    }
-                                                    image={
-                                                        p.image
-                                                    }
-                                                    rate={
-                                                        p
-                                                            .rating
-                                                            .rate
-                                                    }
-                                                    title={
-                                                        p.title
-                                                    }
-                                                    number={
-                                                        nums[
-                                                            ids.indexOf(
-                                                                p.id,
-                                                            )
-                                                        ]
-                                                    }
-                                                    setRerender={
-                                                        setRerender
-                                                    }
-                                                    setNums={
-                                                        setNums
-                                                    }
-                                                />
-                                            ),
-                                        )}
-                                    {!basketProducts && (
-                                        <p>not order</p>
-                                    )}
+                                        basketProducts.map((p) => (
+                                            <Item
+                                                productsInStorage={
+                                                    productsInStorage
+                                                }
+                                                key={p.id}
+                                                item={p}
+                                                increaseProductCount={
+                                                    increaseProductCount
+                                                }
+                                                decreaseProductCount={
+                                                    decreaseProductCount
+                                                }
+                                            />
+                                        ))}
+                                    {!basketProducts && <p>not order</p>}
                                 </div>
                                 <div className="p-4 border border-black/15 rounded-3xl max-md:col-span-full shadow-lg col-span-5">
                                     <Order
-                                        products={
-                                            basketProducts
-                                        }
+                                        products={basketProducts}
                                         nums={nums}
                                         ids={ids}
                                     />
